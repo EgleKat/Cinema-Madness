7ce6ab5d12164b786f9b1a5fa7d3711738f9091c
Unknown <ek1g15@soton.ac.uk>
2018-07-29 10:40:04 +0100

    Implement touch recognition
    Player moves to the touch position

--- Assets/_Scripts/test_red_dot.cs
+++ Assets/_Scripts/test_red_dot.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class test_red_dot : MonoBehaviour {
+    Camera camera;
+    // Use this for initialization
+    void Start () {
+        camera = Camera.main;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    }
 
     public float speed = 0.1F;
 	void Update()
     {
-        if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Moved)
+        if (Input.touchCount == 1 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
+            Touch touch = Input.GetTouch(0);
             // Get movement of the finger since last frame
-            Vector2 touchDeltaPosition = Input.GetTouch(0).deltaPosition;
-
+            Vector3 touchPosinWorldSpace = camera.ScreenToWorldPoint(new Vector3(touch.position.x, touch.position.y, camera.nearClipPlane));
             // Move object across XY plane
-            transform.Translate(-touchDeltaPosition.x * speed, -touchDeltaPosition.y * speed, 0);
+            transform.position = touchPosinWorldSpace;
         }
     }
 
