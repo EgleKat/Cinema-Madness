dbf48905ed69d6f571172aa2d8e8a247e0dbaa27
Unknown <ek1g15@soton.ac.uk>
2018-07-30 21:36:00 +0100

    Make first implementation of NPCs queuing for services

---
+++ Assets/_Scripts/serviceStation.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using UnityEngine;
+using System.Collections.Generic;
+
+public class ServiceStation : MonoBehaviour
+{
+
+    bool inUse;
+    ManageActionQueue playerActionQueue;
+    Queue<ManageNpcActionQueue> waitingNpcs;
+
+    private void Awake()
+    {
+        playerActionQueue = GameObject.FindGameObjectWithTag("Player").GetComponent<ManageActionQueue>();
+        waitingNpcs = new Queue<ManageNpcActionQueue>();
+        inUse = false;
+    }
+    private void unlockObject()
+    {
+        inUse = false;
+    }
+
+    private void lockObject()
+    {
+        inUse = true;
+    }
+    private void OnMouseDown()
+    {
+        playerActionQueue.addToQueue(this);
+    }
+
+    public void enterQueue(ManageNpcActionQueue npcActionQueue)
+    {
+        waitingNpcs.Enqueue(npcActionQueue);
+        acceptNpc();
+    }
+
+    private void acceptNpc()
+    {
+        //Start timer
+        //When timer's finished, pop the person of the queue
+        Debug.Log("Serving NPC");
+        serveFirstNpc();
+    }
+    private void serveFirstNpc()
+    {
+        Debug.Log("Dequeing npc");
+        waitingNpcs.Dequeue().finishTask();
+    }
+
+}
