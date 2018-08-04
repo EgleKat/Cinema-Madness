60145c9a67e8ae851b2d28ae416a234a811f3e9e
Unknown <t3d.y34t3s@gmail.com>
2018-08-04 20:54:49 +0100

    Add Player Movement Script
    Contains methods for starting the player moving, stopping them and changing their destination

---
+++ Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    Pathfinding.AIPath playerAIScript;
+	// Use this for initialization
+	void Awake () {
+        playerAIScript = gameObject.GetComponent<Pathfinding.AIPath>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    /// <summary>
+    /// Set next object to travel to
+    /// </summary>
+    /// <param name="target">The object to travel to</param>
+    public void SetPlayerTarget(ServiceStation target)
+    {
+        gameObject.GetComponent<Pathfinding.AIDestinationSetter>().target = target.gameObject.transform;
+    }
+
+    public void StartMoving()
+    {
+        playerAIScript.canMove = true;
+    }
+
+    public void StopMoving()
+    {
+        playerAIScript.canMove = false;
+    }
+
+}
