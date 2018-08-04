d77d3d416ef00438ddfb01fb817f4c8712555d9b
Unknown <t3d.y34t3s@gmail.com>
2018-08-04 20:33:33 +0100

    Add Player and Grid to Main Scene

---
+++ Assets/_Scripts/StationTrigger.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StationTrigger : MonoBehaviour {
+
+    ManageActionQueue playerQueue;
+	// Use this for initialization
+	void Awake () {
+        playerQueue = GameObject.Find("Player").GetComponent<ManageActionQueue>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnMouseDown()
+    {
+        
+    }
+}
