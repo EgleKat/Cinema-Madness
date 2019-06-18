b77f4fc9ff5787a9d4c4226b42dbf264d2751499
EgleKat <egle.katilauskaite@gmail.com>
2019-06-18 21:53:13 +0100

    npcSpawner generates NPCs based on the wave specification

--- Assets/_Scripts/NpcSpawner.cs
+++ Assets/_Scripts/NpcSpawner.cs
@@ -1,18 +1,22 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NpcSpawner : MonoBehaviour {
+public class NpcSpawner : MonoBehaviour
+{
+    WaveManager waveManager;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    private void Awake()
+    {
+        waveManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<WaveManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
 
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -22,4 +26,27 @@ public class NpcSpawner : MonoBehaviour {
             Destroy(collision.gameObject);
         }
     }
+
+    internal void SpawnNPCs(int numberOfNPCs, float time)
+    {
+        float delayTime = time / numberOfNPCs;
+        StartCoroutine(SpawnNPC(numberOfNPCs, delayTime));
+    }
+    IEnumerator SpawnNPC(int npcsToSpawn, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+        if (npcsToSpawn > 0)
+        {
+            GameObject NPC = Instantiate(Resources.Load("Prefabs/NPC") as GameObject, gameObject.transform);
+            //make the npc a random color
+            NPC.GetComponent<SpriteRenderer>().color = UnityEngine.Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+            npcsToSpawn--;
+            StartCoroutine(SpawnNPC(npcsToSpawn, delayTime));
+        }
+        else
+        {
+            waveManager.WaveFinished();
+        }
+    }
+
 }
--- Assets/_Scripts/WaveManager.cs
+++ Assets/_Scripts/WaveManager.cs
@@ -4,15 +4,57 @@ using UnityEngine;
 
 public class WaveManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    private NpcSpawner spawner;
+    private int currentWave = 1;
+    private Dictionary<int, Wave> allwaves = new Dictionary<int, Wave>
     {
-        
+        {0, new Wave(4,10)},//default wave
+        {1, new Wave(5, 10)},
+        {2, new Wave(3, 10)}
+    };
+
+
+    private void Awake()
+    {
+        spawner = GameObject.FindGameObjectWithTag("Spawn").GetComponent<NpcSpawner>();
+        StartNextWave();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void StartNextWave()
     {
-        
+        Debug.Log("Next Wave " + currentWave);
+                Debug.Log(allwaves.ContainsKey(currentWave));
+
+        if (allwaves.ContainsKey(currentWave))
+        {
+            spawner.SpawnNPCs(allwaves[currentWave].npcNo, allwaves[0].time);
+        }
+        else
+        {
+            Debug.Log("This level does not exist");
+            spawner.SpawnNPCs(allwaves[0].npcNo, allwaves[0].time);
+        }
+    }
+
+    public void WaveFinished()
+    {
+        currentWave++;
+        StartNextWave();
+    }
+    private class Wave
+    {
+        public int npcNo;
+        public float time; //in seconds
+
+        //add other information relevant to waves
+
+        public Wave(int npcNumber, float t)
+        {
+            npcNo = npcNumber;
+            time = t;
+        }
+
     }
 }
+
